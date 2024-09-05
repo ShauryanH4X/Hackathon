296b7ed6fc1a58dbc99c8d49b7dd022c5c6b4030
ShauryanH4X <23mc10sh63@mitsgwl.ac.in>
2024-09-05 23:43:13 +0530

    3rd commit

--- New.js
+++
@@ -1,9 +0,0 @@
-function openNav(){
-    document.getElementById("Sidenav").style.width="250px";
-}
-function closeNav(){
-    document.getElementById("Sidenav").style.width="0";
-}
-function closeNav2(){
-    document.getElementById("Rnav").style.width="0";
-}
